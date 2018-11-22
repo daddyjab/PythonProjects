83b0d7a2fa80dc7b02a63c3ec8b829da65f45d17
Jeffery Brown <39058795+daddyjab@users.noreply.github.com>
2018-11-22 00:39:32 -0600

    Modified the comprehension in the helloworld file

--- helloworld.py
+++ helloworld.py
@@ -5,5 +5,5 @@ Created on Wed Nov 14 19:29:43 2018
 @author: dadja
 """
 
-JEFFLIST = [(x**2, y**3) for x in [1, 2, 3] for y in [1, 2, 3] ]
+JEFFLIST = [(x**3, y**3) for x in [1, 2, 3] for y in [1, 2, 3] ]
 print(JEFFLIST)
