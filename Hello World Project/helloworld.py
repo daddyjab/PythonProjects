01ee08e6736a80ccc752f373487b7f4f72c70c6e
Jeffery Brown <39058795+daddyjab@users.noreply.github.com>
2018-11-30 12:12:52 -0600

    Added prompting for user name

--- Hello World Project/helloworld.py
+++ Hello World Project/helloworld.py
@@ -8,5 +8,13 @@ Created on Wed Nov 14 19:29:43 2018
 #compExample = [(x**3, y**3) for x in [1, 2, 3] for y in [1, 2, 3] ]
 #print(compExample)
 
+# Ask the user's name
+try:
+    uName=str(input("Hey, what's your name? "))
+except:
+    print("Sorry, you really do need to enter a name.")
+    exit()
+
 # Display a "Hello, World" message
-print("Hello, World!")
+print("Hello {uName}, welcome to the World!")
+
